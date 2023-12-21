85f171393c86b449ea6621e5c0c988a50d6106c8
Arshadvk <arshadvk7560@gmail.com>
2023-12-21 15:37:03 +0530

    sign up

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Routes , Route } from "react-router-dom";
+import "./globals.css" ;
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+        <Routes>
+            {/*public routes */}
+
+            <Route element={<AuthLayout/>}>
+            <Route path="/sign-in" element={<SigninForm/>}/>
+            <Route path="/sign-up" element={<SignupForm/>}/>
+            </Route>
+
+            {/*private routes */}
+            <Route element={<RootLayout/>}>
+            <Route index element={<Home/>}/>
+            </Route>
+
+        </Routes>
+    </main>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter} from "react-router-dom"
+import  ReactDOM  from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+)
